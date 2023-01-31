766254259141ecf6027feedc3abeded048ecda47
dushan <dushan.anuradha@grubtech.com>
2023-01-31 20:27:12 +0530

    change config

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -7,7 +7,7 @@ const calOps = new NumericalOperations();
 describe('verify numerical operations between two single digits', () => {
 
   beforeEach('redirect to calculator home page', () => {
-    cy.visit(Cypress.env('baseURL'), { failOnStatusCode: false });
+    cy.visit('/', { failOnStatusCode: false });
     calOps.step_click_AC_btn();
   })
 
