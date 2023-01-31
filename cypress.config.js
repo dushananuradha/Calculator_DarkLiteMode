374ffeb74a225dc862c213a077776e15a23303c9
dushan <dushan.anuradha@grubtech.com>
2023-01-31 19:34:33 +0530

    added cy.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,6 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     projectId: "6g81p2",
+    CYPRESS_RECORD_KEY : "bd2d68ab-bd16-4f23-9a80-bd5fc4cd14d3"
   },
 });
