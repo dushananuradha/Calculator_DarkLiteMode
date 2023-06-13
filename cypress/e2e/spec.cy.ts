4a9d94e571e3efb666b35b89aa8576bd4ec421c1
dushan <dushan.anuradha@grubtech.com>
2023-06-13 08:06:48 +0530

    to test slack notification

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,6 +2,7 @@
 
 import { NumericalOperations } from "../../page-objects/number-pad/numericalOperations";
 
+//important
 const calOps = new NumericalOperations();
 
 describe('verify numerical operations between two single digits', () => {
